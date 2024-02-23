c7f1503941818fb2c466876de17534887e41420c
mottak <kissylamottapereira@gmail.com>
2024-02-23 14:57:23 -0300

    chore: adiciona middleware de erro e rotas de usuário ao servidor

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,5 +1,7 @@
 import express from 'express';
 import 'express-async-errors'
+import { errorMiddleware } from './middleware/errorMiddleware';
+import userRoutes from './routes/userRoutes';
 
 const port = process.env.PORT || 3001
 
@@ -7,6 +9,9 @@ const app = express();
 
 app.use(express.json());
 
+app.use(userRoutes)
+
+app.use(errorMiddleware)
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
