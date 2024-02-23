d0eb05b73a8a5a9df6d03d4e67f41148828d149c
mottak <kissylamottapereira@gmail.com>
2024-02-23 14:30:37 -0300

    chore: cria servidor rodando na porta 3001

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express';
+import 'express-async-errors'
 
 const port = process.env.PORT || 3001
 
 const app = express();
 
 app.use(express.json());
+
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
