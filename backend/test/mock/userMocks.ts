02681571be1f42475ae4949fb25f293320533bad
mottak <kissylamottapereira@gmail.com>
2024-02-24 17:04:58 -0300

    chore: mockes para testes

---
+++ backend/test/mock/userMocks.ts
@@ -0,0 +1,27 @@
+ export const newUser = {
+  name: 'João da Silva',
+  email: 'joao@email.com'
+ }
+
+ export const user = {
+  id: 1,
+  name: 'João da Silva',
+  email: 'joao@email.com'
+ }
+
+ export const allUsers = [{
+  id: 1,
+  name: 'João da Silva',
+  email: 'joao@email.com'
+ },
+ {id: 2,
+ name: 'Maria da Silva',
+ email: 'maria@email.com'
+},
+{
+id: 3,
+name: 'Joana da Silva',
+email: 'joana@email.com'
+},
+
+]
